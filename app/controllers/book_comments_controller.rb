78e921080359c6e535d88b47956ace4863fa6175
yuna096740 <129647184+yuna096740@users.noreply.github.com>
2023-05-28 11:32:32 +0000

    comment ajax

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,16 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
+    @comment = BookComment.find(params[:id])
     BookComment.find(params[:id]).destroy
-    redirect_to book_path(book)
   end
 
   private
