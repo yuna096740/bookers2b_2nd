024a577261a573702d1d0ca8d842014e8d8f2be7
yuna096740 <129647184+yuna096740@users.noreply.github.com>
2023-06-07 02:04:54 +0000

    ajax relationship done

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,24 +3,20 @@ class RelationshipsController < ApplicationController
   def create
     current_user.follow(params[:user_id])
     @user = User.find(params[:user_id])
-    @following_users = @user.following_user
-    @follower_users = @user.follower_user
   end
 
   def destroy
     current_user.unfollow(params[:user_id])
     @user = User.find(params[:user_id])
-    @following_users = @user.following_user
-    @follower_users = @user.follower_user
   end
 
   def follows
-    user = User.find(params[:id])
+    user = User.find(params[:user_id])
     @users = user.following_user
   end
 
   def followers
-    user = User.find(params[:id])
+    user = User.find(params[:user_id])
     @users = user.follower_user
   end
 
